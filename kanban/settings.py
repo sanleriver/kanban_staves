fa1839a59631915a5bf17013e066592165d52eb6
sanleriver <sanleriver@gmail.com>
2025-10-05 16:45:08 +0200

    add json module to deployment

--- kanban/settings.py
+++ kanban/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 import firebase_admin 
 from firebase_admin import credentials, db
+import json
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
